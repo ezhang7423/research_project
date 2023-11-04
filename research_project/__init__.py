2e040ae225d056185dfbf69fe329a5fe1b288728
ezhang7423 <ezhang7423@student.palomar.edu>
2023-11-04 18:55:32 -0400

    add run dir log dir

--- research_project/__init__.py
+++ research_project/__init__.py
@@ -29,11 +29,12 @@ __version__ = version
 
 REPO_DIR = setup_path(Path(abspath()) / "..")
 DATA_ROOT = setup_path(os.getenv('DATA_ROOT') or  REPO_DIR / "data")
-LOG_DIR = setup_path(DATA_ROOT / 'runs' / datestr())
+RUN_DIR = setup_path(DATA_ROOT / 'runs')
+LOG_DIR = setup_path(RUN_DIR / datestr())
 
 
 print(f"LOG DIR: {LOG_DIR}")
 
 if not (REPO_DIR / 'runs').exists():
-    print(f'Creating symlink from {REPO_DIR / "runs"} to {LOG_DIR}')
-    (REPO_DIR / 'runs').symlink_to(DATA_ROOT / 'runs')
+    print(f'Creating symlink from {REPO_DIR / "runs"} to {RUN_DIR}')
+    (REPO_DIR / 'runs').symlink_to(RUN_DIR)
